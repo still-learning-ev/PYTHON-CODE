bf12aeaf2cbe8c79768998f33c85bac7cecc5c19
zeeshan <through.learning@gmail.com>
2023-03-22 21:36:53 +0530

    added parenthesis checker

--- DSA/string.py
+++ DSA/string.py
@@ -15,6 +15,34 @@ class StringManipulation:
     def find_in_string(self, String:str, key:str):
         pass
 
+    def longest_palindrome(self, string:str):
+        pass
+
+    def helper_longest_plaindrome(self, string:str):
+        pass
+
+    def parenthesis_checker(self, string:str):
+        # Returns true if the parenthesis is balanced
+        arr = []
+        opening = ['{', '[', '(']
+        closing = ['}', ']', ')']
+        
+        for i in string:
+            if len(arr) == 0:
+                arr.append(i)
+            elif arr[-1] in opening and i in closing:
+                if opening.index(arr[-1]) == closing.index(i):
+                    arr.pop()
+                else:
+                    arr.append(i)
+            else:
+                arr.append(i)
+                
+        if len(arr) == 0:
+            return True
+        return False
+
+
 
 
 if __name__=="__main__":
@@ -23,5 +51,7 @@ if __name__=="__main__":
     print(f"The reverse of the string is {reverse} .")
     is_anagram = "" if obj.is_anagram("hello", "ollleh") else " not"
     print(f"The strings provided are{is_anagram} anagrams of each other.")
+    is_balanced = "balanced" if obj.parenthesis_checker("{()}}") else "not balanced"
+    print(f"The given string is {is_balanced}")
 
 
