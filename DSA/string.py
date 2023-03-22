788dfdee61f87c04ef18844850556bfcaf572cba
zeeshan <through.learning@gmail.com>
2023-03-22 22:15:41 +0530

    Added function fro longest palindrome

--- DSA/string.py
+++ DSA/string.py
@@ -1,4 +1,7 @@
 class StringManipulation:
+    def __init__(self) -> None:
+        self.l = []
+
     def reverse_string(self, string:str):
         # Returns the reverse of a string
         return string[-1::-1]
@@ -41,7 +44,36 @@ class StringManipulation:
         if len(arr) == 0:
             return True
         return False
+    
 
+    def longestPalin(self, S):
+        self.helper(S, self.l)
+        max =-9999
+        ans = ""
+        for i in self.l:
+            if len(i)>max:
+                max = len(i)
+                ans = i
+        # print(self.l)  
+        return ans
+    
+    def helper(self, s, l):
+        # Base case
+        if self.is_palindrome(s):
+            # print(s)
+            self.l.append(s)
+        if len(s)==1:
+            return
+        # recursive step
+
+        self.helper(s[:len(s)-1], l)
+        self.helper(s[1:], l)
+    
+    def is_palindrome(self, s):
+        if s[-1::-1]==s:
+            return True
+        return False
+    
 
 
 
@@ -53,5 +85,6 @@ if __name__=="__main__":
     print(f"The strings provided are{is_anagram} anagrams of each other.")
     is_balanced = "balanced" if obj.parenthesis_checker("{()}}") else "not balanced"
     print(f"The given string is {is_balanced}")
+    print(obj.longestPalin("aaaabbsdjkddsaaaaddaaaa"))
 
 
